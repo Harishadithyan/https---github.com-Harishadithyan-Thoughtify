7bd16a34ec9e259a9a4c49338a4543d6fc7f57b2
Harishadithyan <harishadithyan17@gmail.com>
2024-12-02 18:03:45 +0530

    comit

--- pro1/pro1/settings.py
+++ pro1/pro1/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@$^kl=hnsk(@n&n^ryak+#bh&-fqfr$bob(rnma78%m5fv1$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
